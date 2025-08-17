e701fdcb4e441dba232235c8431bffa20e674ed0
Roy Bipro <roybipro896@gmail.com>
2025-08-17 20:19:31 +0600

    Update Program.cs

--- 2nd Assigenment/2nd Assigenment/Program.cs
+++ 2nd Assigenment/2nd Assigenment/Program.cs
@@ -100,10 +100,10 @@ class Program
 {
     static void Main(string[] args)
     {
-        int j = 1;
+        
         for (int i = 1; i < 6; i++)
         {
-            for (j = 1; j < i; j++)
+            for (int j = 1; j < i; j++)
             {
                 Console.Write(" ");
             }
