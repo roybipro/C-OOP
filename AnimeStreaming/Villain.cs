fd53e277ab635c108ef77ca39e7425d130b16cf0
Roy Bipro <roybipro896@gmail.com>
2025-08-17 23:45:40 +0600

    Update Villain.cs

--- AnimeStreaming/Villain.cs
+++ AnimeStreaming/Villain.cs
@@ -6,30 +6,33 @@ using System.Threading.Tasks;
 
 namespace AnimeStreaming
 {
-    internal class Villain : Character
+   internal class Villain : Character
     {
-        int evilScore;
-        string finalBattleOutcome;
+        private int evilScore;
+        private string finalBattleOutcome;
 
-        public Villain()
+        public Villain() { }
+
+        public Villain(string characterID, string characterName, string animeName,
+                       int evilScore, string finalBattleOutcome)
+            : base(characterID, characterName, animeName)
         {
+            this.evilScore = evilScore;
+            this.finalBattleOutcome = finalBattleOutcome;
         }
-        public Villain(string characterID, string characterName, string animeName,int evilScore, string finalBattleOutcome): base(characterID, characterName, animeName)
-     
-        {
-            this.evilScore = EvilScore;
-            this.finalBattleOutcome = FinalBattleOutcome;
-        }
+
         public int EvilScore
         {
             get { return evilScore; }
             set { evilScore = value; }
         }
+
         public string FinalBattleOutcome
         {
             get { return finalBattleOutcome; }
             set { finalBattleOutcome = value; }
         }
+
         public override void ShowInfo()
         {
             base.ShowInfo();
