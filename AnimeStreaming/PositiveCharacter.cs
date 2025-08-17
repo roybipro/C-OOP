bb203cc61e1d3b3a2f438632e7f0bc5783709324
Roy Bipro <roybipro896@gmail.com>
2025-08-17 23:39:22 +0600

    Update PositiveCharacter.cs

--- AnimeStreaming/PositiveCharacter.cs
+++ AnimeStreaming/PositiveCharacter.cs
@@ -1,44 +1,90 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System;
 
 namespace AnimeStreaming
 {
+    // Base Character class
+    internal class Character
+    {
+        private string characterID;
+        private string characterName;
+        private string animeName;
+
+        public Character() { }
+
+        public Character(string characterID, string characterName, string animeName)
+        {
+            this.CharacterID = characterID;
+            this.CharacterName = characterName;
+            this.AnimeName = animeName;
+        }
+
+        public string CharacterID
+        {
+            get { return characterID; }
+            set { characterID = value; }
+        }
+
+        public string CharacterName
+        {
+            get { return characterName; }
+            set { characterName = value; }
+        }
+
+        public string AnimeName
+        {
+            get { return animeName; }
+            set { animeName = value; }
+        }
+
+        public virtual void ShowInfo()
+        {
+            Console.WriteLine("Character ID: " + CharacterID);
+            Console.WriteLine("Character Name: " + CharacterName);
+            Console.WriteLine("Anime Name: " + AnimeName);
+        }
+
+        public virtual bool CheckPopularity()
+        {
+            return false;
+        }
+    }
+
+    // Derived PositiveCharacter class
     internal class PositiveCharacter : Character
     {
-        int powerLevel;
-        string isMainCharacter;
-        int numberOfEpisodes;
+        private int powerLevel;
+        private string isMainCharacter;
+        private int numberOfEpisodes;
 
-        public PositiveCharacter()
-        {
-            
-        }
+        public PositiveCharacter() { }
+
         public PositiveCharacter(string characterID, string characterName, string animeName,
                                  int powerLevel, string isMainCharacter, int numberOfEpisodes)
             : base(characterID, characterName, animeName)
         {
-            this.powerLevel = PowerLevel;
-            this.isMainCharacter = IsMainCharacter;
-            this.numberOfEpisodes = NumberOfEpisodes;
+            this.PowerLevel = powerLevel;
+            this.IsMainCharacter = isMainCharacter;
+            this.NumberOfEpisodes = numberOfEpisodes;
         }
+
         public int PowerLevel
         {
             get { return powerLevel; }
             set { powerLevel = value; }
         }
+
         public string IsMainCharacter
         {
             get { return isMainCharacter; }
             set { isMainCharacter = value; }
         }
+
         public int NumberOfEpisodes
         {
             get { return numberOfEpisodes; }
             set { numberOfEpisodes = value; }
         }
+
         public override bool CheckPopularity()
         {
             return IsMainCharacter == "Yes" && PowerLevel > 9000 && NumberOfEpisodes > 150;
@@ -54,4 +100,13 @@ namespace AnimeStreaming
         }
     }
 
+    // Example usage
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PositiveCharacter pc = new PositiveCharacter("C001", "Naruto Uzumaki", "Naruto", 9500, "Yes", 220);
+            pc.ShowInfo();
+        }
+    }
 }
